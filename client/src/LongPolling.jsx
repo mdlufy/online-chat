699b59a492641300ec60a87fc406d98e0403d162
mdlufy <german.panov.2018@mail.ru>
2022-09-07 11:46:49 +0300

    feat: messages render and server message send handle

--- client/src/LongPolling.jsx
+++ client/src/LongPolling.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 function LongPolling(props) {
+    const [messages, setMessages] = useState([]);
+    const [value, setValue] = useState('');
+
+    async function sendMessage() {
+        await axios.post('http://localhost:5000/new-messages', {
+            id: Date.now(),
+            message: value,
+        })
+    }
+
     return (
-        <div className='center'>
+        <div className="center">
             <div>
-                <div className='form'>
-                    <input type='text' />
-                    <button>Отправить</button>
+                <div className="form">
+                    <input value={value} onChange={e => setValue(e.target.value)} type="text" />
+                    <button onClick={sendMessage}>Отправить</button>
+                </div>
+                <div className="messages">
+                    {messages.map(message => 
+                        <div className="message" key={message.id}>
+                            {message.value}
+                        </div>     
+                    )}
                 </div>
             </div>
         </div>
