1a2f3d1c29c77e7686fc2c3ae06c70cc566c2bcf
mdlufy <german.panov.2018@mail.ru>
2022-09-07 13:27:59 +0300

    feat: render WebSock comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LongPolling from './LongPolling';
 import EventSourcing from './EventSourcing';
+import WebSock from './WebSock';
 
 function App() {
   return (
       <>
        {/* <LongPolling /> */}
-        <EventSourcing />
+        {/* <EventSourcing /> */}
+        <WebSock />
       </>
   );
 }
