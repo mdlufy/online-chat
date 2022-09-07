55bd9c548ac550c73ce0a39a8555bd195a190030
mdlufy <german.panov.2018@mail.ru>
2022-09-07 10:46:32 +0300

    initial commit

---
+++ client/src/LongPolling.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LongPolling(props) {
+    return (
+        <div className='center'>
+            <div>
+                <div className='form'>
+                    <input type='text' />
+                    <button>Отправить</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LongPolling;
---
+++ server/longpolling.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const event = require('events');
+
+const PORT = 5000;
+
+const emitter = new event.EventEmitter();
+
+const app = express();
+
+app.use(cors())
+
+app.get('get-messages', ((req, res) => {
+    emitter.once('newMessage', (message) => {
+        res.json(message);
+    })
+}))
+
+app.post('new-messages', ((req, res) => {
+    const message = req.body;
+
+    emitter.emit('newMessages', message);
+
+    res.status(200)
+}))
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`))
