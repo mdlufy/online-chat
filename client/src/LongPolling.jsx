9918647dfd1e8307ab36ee46f8e4ced085975396
mdlufy <german.panov.2018@mail.ru>
2022-09-07 12:21:15 +0300

    feat: add subscribe on new message event

--- client/src/LongPolling.jsx
+++ client/src/LongPolling.jsx
@@ -1,14 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-function LongPolling(props) {
+function LongPolling() {
     const [messages, setMessages] = useState([]);
     const [value, setValue] = useState('');
+
+    useEffect(() => {
+        subscribe();
+    }, [])
+
+    async function subscribe() {
+        try {
+            const {data} = await axios.get('http://localhost:5000/get-messages');
+
+            setMessages(prev => [data, ...prev]);
+
+            await subscribe();
+        } catch (e) {
+            setTimeout(() => {
+                subscribe();
+            }, 500)
+        }
+    }
 
     async function sendMessage() {
         await axios.post('http://localhost:5000/new-messages', {
             id: Date.now(),
-            message: value,
+            messageText: value,
         })
     }
 
@@ -22,7 +40,7 @@ function LongPolling(props) {
                 <div className="messages">
                     {messages.map(message => 
                         <div className="message" key={message.id}>
-                            {message.value}
+                            {message.messageText}
                         </div>     
                     )}
                 </div>
