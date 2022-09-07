fcbf42110b49aec1a236d8ea1229794a17c5864f
mdlufy <german.panov.2018@mail.ru>
2022-09-07 11:43:10 +0300

    fix: correct endpoint

--- server/longpolling.js
+++ server/longpolling.js
@@ -10,13 +10,13 @@ const app = express();
 
 app.use(cors())
 
-app.get('get-messages', ((req, res) => {
+app.get('/get-messages', ((req, res) => {
     emitter.once('newMessage', (message) => {
         res.json(message);
     })
 }))
 
-app.post('new-messages', ((req, res) => {
+app.post('/new-messages', ((req, res) => {
     const message = req.body;
 
     emitter.emit('newMessages', message);
