529a486bba5782834a46a964bdaeecaf1bcf616a
mdlufy <german.panov.2018@mail.ru>
2022-10-23 05:15:32 +0300

    feat: statuses exchange

--- client/src/WebSock.jsx
+++ client/src/WebSock.jsx
@@ -1,94 +1,140 @@
-import React, { useRef, useState } from 'react';
-import axios from 'axios';
+import React, { useRef, useState } from "react";
 
 function WebSock() {
     const [messages, setMessages] = useState([]);
-    const [value, setValue] = useState('');
+    const [value, setValue] = useState("");
     const socket = useRef();
+    const [currWriters, setCurrWriters] = useState([]);
     const [isConnected, setConnected] = useState(false);
-    const [username, setUsername] = useState('');
-
+    const [username, setUsername] = useState("");
 
     async function sendMessage() {
         const message = {
             username,
             text: value,
             id: Date.now(),
-            event: 'message',
-        }
+            event: "message",
+        };
 
         socket.current.send(JSON.stringify(message));
-        
-        setValue('');
+
+        setValue("");
+    }
+
+    async function sendStatus() {
+        const status = {
+            username,
+            id: Date.now(),
+            event: "changeStatus",
+        };
+
+        socket.current.send(JSON.stringify(status));
+    }
+
+    function startWrite(e) {
+        setValue(e.target.value);
+
+        if (currWriters.includes(username)) {
+            e.stopPropagation();
+        } else {
+            sendStatus();
+        }
     }
 
     function connect() {
-        socket.current = new WebSocket('ws://localhost:5000');
+        socket.current = new WebSocket("ws://localhost:5000");
 
         socket.current.onopen = () => {
             setConnected(true);
 
             const message = {
-                event: 'connection',
+                event: "connection",
                 username,
                 id: Date.now(),
-            }
+            };
 
             socket.current.send(JSON.stringify(message));
-        }
+        };
 
         socket.current.onmessage = (event) => {
             const response = event.data;
             const data = JSON.parse(response);
 
-            setMessages(prev => [data, ...prev]);
-        }
+            if (data.event === "changeStatus") {
+                if (!currWriters.includes(data.username)) {
+                    setCurrWriters((writers) => [...writers, data.username]);
+                }
+
+                setTimeout(() => {
+                    setCurrWriters((writers) =>
+                        writers.filter((name) => name !== data.username)
+                    );
+                }, 2000);
+            }
+
+            setMessages((prev) => [...prev, data]);
+        };
 
         socket.current.onclose = () => {
-            console.log('Socket закрыт');
-        }
+            console.log("Socket закрыт");
+        };
 
         socket.current.onerror = () => {
-            console.log('Socket произошла ошибка');
-        }
+            console.log("Socket произошла ошибка");
+        };
     }
 
     if (!isConnected) {
         return (
-            <div className='center'>
-                <div className='form'>
-                    <input 
-                        value={username} 
-                        onChange={e => setUsername(e.target.value)} 
-                        type='text' 
-                        placeholder='Введите ваше имя'
+            <div className="center">
+                <div className="form">
+                    <input
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        type="text"
+                        placeholder="Введите ваше имя"
                     />
                     <button onClick={connect}>Войти</button>
                 </div>
             </div>
-        )
+        );
     }
 
     return (
         <div className="center">
             <div>
                 <div className="form">
-                    <input value={value} onChange={e => setValue(e.target.value)} type="text" placeholder="Введите сообщение" />
+                    <input
+                        value={value}
+                        onChange={(e) => startWrite(e)}
+                        on
+                        type="text"
+                        placeholder="Введите сообщение"
+                    />
                     <button onClick={sendMessage}>Отправить</button>
                 </div>
                 <div className="messages">
-                    {messages.map(message => 
+                    {messages.map((message) => (
                         <div key={message.id}>
-                            {message.event === 'connection'
-                                ? <div className='connection_message'>
+                            {message.event === "connection" && (
+                                <div className="connection_message">
                                     Пользователь {message.username} подключился
                                 </div>
-                                : <div className='message'>
+                            )}
+                            {message.event === "message" && (
+                                <div className="message">
                                     {message.username}: {message.text}
                                 </div>
-                            }
-                        </div>     
-                    )}
+                            )}
+                        </div>
+                    ))}
+                </div>
+                <div>
+                    {currWriters.map((writer) => (
+                        <div key={writer.id}>
+                            <div>{writer} печатает...</div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
