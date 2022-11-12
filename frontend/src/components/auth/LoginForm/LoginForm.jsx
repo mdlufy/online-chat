8889bf34a7c3da8da7abe2cb31d4e60cdbe6e8f0
mdlufy <german.panov.2018@mail.ru>
2022-11-12 23:51:37 +0300

    feat: add send messages by enter key

--- frontend/src/components/auth/LoginForm/LoginForm.jsx
+++ frontend/src/components/auth/LoginForm/LoginForm.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 function LoginForm({username, setUsername, connect}) {
+    function handleKeyDown(e) {
+        if (e.key === "Enter") {
+            connect();
+        }
+    }
+
     return (
         <div className="center">
             <div className="form">
                 <input
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
+                    onKeyDown={(e) => handleKeyDown(e)}
                     type="text"
                     placeholder="Введите ваше имя"
                 />
--- frontend/src/components/textMessage/MessageForm/MessageForm.jsx
+++ frontend/src/components/textMessage/MessageForm/MessageForm.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 function MessageForm({value, startWrite, sendMessage}) {
+    function handleKeyDown(e) {
+        if (e.key === "Enter") {
+            sendMessage();
+        }
+    }
+
     return (
         <div className="form">
             <input
                 value={value}
                 onChange={(e) => startWrite(e)}
+                onKeyDown={(e) => handleKeyDown(e)}
                 type="text"
                 placeholder="Введите сообщение"
             />
